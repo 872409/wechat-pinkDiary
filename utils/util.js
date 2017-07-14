623c94cd2064e2871d622a4129b9ab56023c0e3f
unknown <>
2017-07-14 16:45:03 +0800

    工具文件

---
+++ utils/util.js
@@ -0,0 +1,39 @@
+var ossAliyuncs = "http://soupu.oss-cn-shanghai.aliyuncs.com";
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+function getNowFormatDate() {
+        var date = new Date();
+        var seperator1 = "-";
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var strDate = date.getDate();
+        if (month >= 1 && month <= 9) {
+            month = "0" + month;
+        }
+        if (strDate >= 0 && strDate <= 9) {
+            strDate = "0" + strDate;
+        }
+        var currentdate = year  +seperator1 + month+ seperator1 + strDate;
+        return currentdate;
+    }
+
+module.exports = {
+  formatTime: formatTime,
+  ossAliyuncs: ossAliyuncs,
+  getNowFormatDate:getNowFormatDate
+}
